894b880747f6a92e1d610f541bbaaac0126e2b76
VictorHTenorio <victorhorten7@gmail.com>
2024-05-22 21:06:53 -0300

    Author: VictorHTenorio <victorhorten7@gmail.com>
    mesa done

--- src/main/java/org/johnny/repository/MesaRepository.java
+++ src/main/java/org/johnny/repository/MesaRepository.java
@@ -1,11 +1,14 @@
 package org.johnny.repository;
 
+import org.johnny.models.Dependente;
 import org.johnny.models.Mesa;
+import org.johnny.models.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -36,5 +39,13 @@ public class MesaRepository {
         final String sql = "delete from mesa where id_mesa = ?";
 
         jdbcTemplate.update(sql, id_mesa);
+    }
+
+    public void update(int id_mesa, Mesa mesa){
+        final String sql = "update mesa set id_mesa = ? where id_mesa = ?";
+
+        jdbcTemplate.update(sql,
+               mesa.getId_mesa(),
+                id_mesa);
     }
 }
